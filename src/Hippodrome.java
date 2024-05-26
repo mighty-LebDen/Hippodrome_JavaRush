6ae50399fa4ad1e30e8b4cd570ce28bae8820884
Denis Lebedev <lebedev.dm00@mail.ru>
2024-05-26 20:19:06 +0900

    Add initialization to Hippodrome

--- src/Hippodrome.java
+++ src/Hippodrome.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
   private List<Horse> horses;
+  private static Hippodrome game;
   
   public List<Horse> getHorses() {
     return this.horses;
@@ -10,7 +12,13 @@ public class Hippodrome {
     this.horses = horses;
   }
   public static void main(String[] args) {
-  
-  
+    Horse firstHorse = new Horse("horseOne", 3, 0);
+    Horse secondHorse = new Horse("horseTwo", 3, 0);
+    Horse thirdHorse = new Horse("horseThree", 3, 0);
+    List<Horse> horseList = new ArrayList<>();
+    horseList.add(firstHorse);
+    horseList.add(secondHorse);
+    horseList.add(thirdHorse);
+    Hippodrome.game = new Hippodrome(horseList);
   }
 }
