00952e1ffc26184a46831ab99837a7e247348ae0
Denis Lebedev <lebedev.dm00@mail.ru>
2024-05-26 19:52:49 +0900

    Add horses to Hippodrome

--- src/Hippodrome.java
+++ src/Hippodrome.java
@@ -1,5 +1,12 @@
+import java.util.List;
+
 public class Hippodrome {
+  private List<Horse> horses;
+  public List<Horse> getHorses() {
+    return this.horses;
+  }
   public static void main(String[] args) {
+  
   
   }
 }
