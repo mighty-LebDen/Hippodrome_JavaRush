40354234817183054792cacffc779009c191120c
Denis Lebedev <lebedev.dm00@mail.ru>
2024-05-26 19:59:35 +0900

    Add constructor for Hippodrome

--- src/Hippodrome.java
+++ src/Hippodrome.java
@@ -2,8 +2,12 @@ import java.util.List;
 
 public class Hippodrome {
   private List<Horse> horses;
+  
   public List<Horse> getHorses() {
     return this.horses;
+  }
+  public Hippodrome(List<Horse> horses) {
+    this.horses = horses;
   }
   public static void main(String[] args) {
   
